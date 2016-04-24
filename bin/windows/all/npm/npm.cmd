e1852be2626d67f5e3f62d7ab28e06b2efe599ce
Croydon <git@rvapps.org>
2016-04-24 18:51:39 +0200

    Define NODE_EXE in tool/

--- bin/windows/all/npm/npm.cmd
+++ bin/windows/all/npm/npm.cmd
@@ -1,12 +1,5 @@
-:: Created by npm, please don't edit manually.
-@ECHO OFF
-
-SETLOCAL
-
-SET "NODE_EXE=%~dp0\node.exe"
-IF NOT EXIST "%NODE_EXE%" (
-  SET "NODE_EXE=node"
-)
+:: modified by Inexor
+:: called by tool/node_windows_generic_handler.bat
 
 SET "NPM_CLI_JS=%~dp0\node_modules\npm\bin\npm-cli.js"
 FOR /F "delims=" %%F IN ('CALL "%NODE_EXE%" "%NPM_CLI_JS%" prefix -g') DO (
